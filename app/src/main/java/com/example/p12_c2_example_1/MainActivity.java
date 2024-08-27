b4bb665ba6ea2ff67ef02b2dbb692f77fb907067
joaonunez <joaovaldiglesias@gmail.com>
2024-08-27 09:21:28 -0400

    sumar ready

--- app/src/main/java/com/example/p12_c2_example_1/MainActivity.java
+++ app/src/main/java/com/example/p12_c2_example_1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.p12_c2_example_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,28 +12,40 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tvRes;
     EditText etVal1, etVal2;
+    Button etButtonSumar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Inicio
+
         tvRes = findViewById(R.id.tvResultado);
         etVal1 = findViewById(R.id.etValor1);
         etVal2 = findViewById(R.id.etValor2);
+        etButtonSumar = findViewById(R.id.buttonSumar);
 
         //FIN
+
+        etButtonSumar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                sumar();
+            }
+        });
     }
 
-    public void sumar(View view){
-        int x = Integer.parseInt(etVal1.getText().toString());
-        int y = Integer.parseInt(etVal2.getText().toString());
-        int r = x+y;
 
-        if(r<0){
-
+    public void sumar(){
+        try {
+            int x = Integer.parseInt(etVal1.getText().toString());
+            int y = Integer.parseInt(etVal2.getText().toString());
+            int r = x+y;
+            tvRes.setText("La Suma Es: " + r);
+        } catch (NumberFormatException e){
+            tvRes.setText("Ingrese valores validos");
         }
 
-        tvRes.setText(String.valueOf(r));
+
     }
 }
 //agregar un valor correspondiente a una distancia especifica
